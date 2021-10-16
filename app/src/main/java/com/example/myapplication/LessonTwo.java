8791184039d8ff407f80cc5e79a685ca9692627f
AlexIvanov2021 <ivanovzpua1993@gmail.com>
2021-10-16 22:52:33 +0300

    Lesson 1-2-2

---
+++ app/src/main/java/com/example/myapplication/LessonTwo.java
@@ -0,0 +1,28 @@
+package com.example.myapplication;
+
+public class LessonTwo {
+
+    int id ;
+    String s;
+
+    public LessonTwo(int id, String s) {
+        this.id = id;
+        this.s = s;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getS() {
+        return s;
+    }
+
+    public void setS(String s) {
+        this.s = s;
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,62 +10,25 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView textView;
-    Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = findViewById(R.id.text_view);
-        button = findViewById(R.id.button);
+        textView = findViewById(R.id.result);
 
-        textView.append("onCreate " +"\n");
+        LessonTwo lessonTwo_one = new LessonTwo(1, "a" );
+        LessonTwo lessonTwo_two = new LessonTwo(1,"a");
 
-   button.setOnClickListener(new View.OnClickListener() {
-       @Override
-       public void onClick(View view) {
-           Intent intent = new Intent(MainActivity.this,SecondActivity.class);
-           startActivity(intent);
-       }
-   });
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        textView.append("onStart " +"\n");
+        textView.append("hashCode()lessonTwo_one"+lessonTwo_one.hashCode()+"\n" );
+        textView.append("hashCode() lessonTwo_two"+ lessonTwo_two.hashCode()+"\n");
+        textView.append("" + lessonTwo_one.equals(lessonTwo_two));
 
 
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        textView.append("onResume " +"\n");
-
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        textView.append("onPause " +"\n");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        textView.append("onStop " +"\n");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        textView.append("onDestroy " +"\n");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        textView.append("onRestart " +"\n");
-    }
 }
+
+
+
